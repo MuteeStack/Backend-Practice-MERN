a917b86dd900669d35b7b62bb827b8a55949099b
MuteeStack <muteestacks@gmail.com>
2026-07-30 18:04:44 +0500

    Complete Todo Data Modeling

--- models/todos/todo.models.js
+++ models/todos/todo.models.js
@@ -1,6 +1,23 @@
 import mongoose from "mongoose"
 const todoSchema = mongoose.Schema({
-
+    content: {
+        type: String,
+        required: true
+    },
+    complete : {
+        type: String,
+        default: false
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    subTodos: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "SubTodo"
+        }
+    ]
 },
 {
     timestamps: true

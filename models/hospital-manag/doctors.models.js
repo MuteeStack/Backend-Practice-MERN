a18c178651865b7080c88422fac0a623f1ec08e2
MuteeStack <muteestacks@gmail.com>
2026-07-31 12:16:36 +0500

    Added Dotoe and Hospital Model

--- models/hospital-manag/doctors.models.js
+++ models/hospital-manag/doctors.models.js
@@ -1,7 +1,32 @@
 import mongoose from "mongoose"
 
 const doctorSchema = new mongoose.Schema({
-    
+    name: {
+        type: String,
+        required: true
+    },
+    salary: {
+        type: String,
+        required: true
+    },
+    specialization: {
+        type: String,
+        required: true
+    },
+    qualifications: {
+        type: String,
+        required: true
+    },
+    Experience: {
+        type: Number,
+        default: 0
+    },
+    worksIn: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Hospital"
+        }
+    ]
 })
 
 export const Doctor = mongoose.model("Doctor" , doctorSchema)
--- models/hospital-manag/hospital.models.js
+++ models/hospital-manag/hospital.models.js
@@ -1,7 +1,14 @@
 import mongoose from "mongoose"
 
 const hospitalSchema = new mongoose.Schema({
-    
+    name: {
+        type: String,
+        required: true
+    },
+    location:{
+        type: String,
+        required: true
+    }
 })
 
 export const Hospital = mongoose.model("Hospital" , hospitalSchema)
